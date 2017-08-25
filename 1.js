8883ce790e01d538a27e0c0199e8cbd4a8d6d9d9
Anamaria <amic_dragic@yahoo.com>
2017-08-25 16:52:28 +0200

    This is my blog

---
+++ 1.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+    $(".move").mouseover(function(){
+        $(".move").animate({left: '2px'} , "fast");
+		$(".move").stop();
+    });
+   $(".move").mouseout(function(){
+        $(".move").animate({left: '0px'} , "fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".move1").mouseover(function(){
+        $(".move1").animate({left: '2px'} , "fast");
+		$(".move1").stop();
+    });
+   $(".move1").mouseout(function(){
+        $(".move1").animate({left: '0px'} , "fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".move2").mouseover(function(){
+        $(".move2").animate({left: '2px'} , "fast");
+		$(".move2").stop();
+    });
+   $(".move2").mouseout(function(){
+        $(".move2").animate({left: '0px'} , "fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".move3").mouseover(function(){
+        $(".move3").animate({left: '2px'} , "fast");
+		$(".move3").stop();
+    });
+   $(".move3").mouseout(function(){
+        $(".move3").animate({left: '0px'} , "fast");
+    });
+});
